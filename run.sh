819d66ff3514f10e8815d424a303d548665e046d
root <dominic.mueller.90@gmail.com>
2014-05-24 22:10:21 +0200

    added license

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-gcc -std=c99 -Wall mem.c -o mem -lm 
+gcc -std=c99 -Wall recgen.c -o recgen -lm 
 rm test*
-for x in {1..1000} ; do ./mem test$x.ppm $x; 
+for x in {1..1000} ; do ./recgen test$x.ppm $x; 
 
 if [ -f test$x.ppm ]; then
 	pnmtopng test$x.ppm > test$x.png; 
