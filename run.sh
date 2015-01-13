69f471167429eda575998fd76b56f86657583d91
dominic müller <dominic.mueller.90@gmail.com>
2015-01-13 02:16:02 +0100

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+rm recgen
 gcc -std=c99 -Wall recgen.c -o recgen -lm 
 rm test*
-for x in {1..1000} ; do ./recgen test$x.ppm $x; 
+for x in {1000..5000} ; do ./recgen test$x.ppm $x; 
 
 if [ -f test$x.ppm ]; then
-	pnmtopng test$x.ppm > test$x.png; 
+	pnmtopng test$x.ppm > output/test$x.png; 
 fi
 
 rm test$x.ppm;
